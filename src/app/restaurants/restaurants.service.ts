cc78150132feacfc00657205e6b5c48b08440566
Juda Emanuel <judahemanuel@gmail.com>
2019-06-06 14:25:21 -0300

    commit after lesson 48

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 
-import { Restaurant } from "./restaurant/restaurant.model";
-
-import { MEAT_API } from "app/app.api";
 import { Observable } from 'rxjs/Observable';
 import "rxjs/add/operator/map";
+import "rxjs/add/operator/catch";
+
+import { Restaurant } from "./restaurant/restaurant.model";
+import { MEAT_API } from "app/app.constants";
+import { ErrorHandler } from 'app/app.error-handler';
 
 @Injectable()
 export class RestaurantsService {
@@ -14,6 +16,13 @@ export class RestaurantsService {
 
   restaurants(): Observable<Restaurant[]> {
     return this.http.get(`${MEAT_API}/restaurants`)
-      .map(resp => resp.json());
+      .map(response => response.json())
+      .catch(ErrorHandler.handleError);
+  }
+
+  restaurantById(id: string): Observable<Restaurant> {
+    return this.http.get(`${MEAT_API}/restaurants/${id}`)
+      .map(response => response.json())
+      .catch(ErrorHandler.handleError);
   }
 }
