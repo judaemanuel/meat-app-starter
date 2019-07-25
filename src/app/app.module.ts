3c4486fc95258baf39291f607ead631433477e78
Juda Emanuel <judahemanuel@gmail.com>
2019-07-25 14:33:50 -0300

    Aula 52

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { RouterModule } from '@angular/router';
 
 import { ROUTES } from './app.route';
 
+import { RestaurantsService } from './restaurants/restaurants.service';
+import { ShoppingCartService } from './restaurants/restaurant-detail/shopping-cart/shopping-cart.service';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { RestaurantsComponent } from './restaurants/restaurants.component';
 import { RestaurantComponent } from './restaurants/restaurant/restaurant.component';
-import { RestaurantsService } from './restaurants/restaurants.service';
 import { RestaurantDetailComponent } from './restaurants/restaurant-detail/restaurant-detail.component';
 import { MenuComponent } from './restaurants/restaurant-detail/menu/menu.component';
 import { MenuItemComponent } from './restaurants/restaurant-detail/menu-item/menu-item.component';
@@ -40,7 +42,7 @@ import { ReviewComponent } from './restaurants/restaurant-detail/reviews/review/
     HttpModule,
     RouterModule.forRoot(ROUTES)
   ],
-  providers: [RestaurantsService],
+  providers: [RestaurantsService, ShoppingCartService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
