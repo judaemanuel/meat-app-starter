bc53f13f4c376f032b950bd19d0063a918cd759f
Judá Emanuel <juda@seugestordigital.com.br>
2020-10-16 23:35:24 -0300

    change to adjust error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -50,7 +50,7 @@ import { LoginComponent } from './security/login/login.component';
     RouterModule.forRoot(ROUTES, { preloadingStrategy: PreloadAllModules }),
     SharedModule.forRoot()
   ],
-  providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }],
+  providers: [/*{ provide: LocationStrategy, useClass: HashLocationStrategy }*/],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
